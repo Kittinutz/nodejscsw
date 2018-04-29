0418f0aee332180136651c10094657912d6bc37e
Chakkrit Chueangyang <30730045+Him-2C@users.noreply.github.com>
2018-04-30 05:08:37 +0700

    Update

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import router from './routes'
-import cors from 'cors'
-const Userroute = require('./Routes/Userroute');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')  // เขียนมั้ง
+const cors = require('cors')               // เขียนมั้ง
+const router = express.Router()            // เขียนมั้ง
 
-const app = express();
-const PORT = process.env.PORT || 3000
-app.use(cors());
-app
-  // NOTE: pass json data
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
+let bears = [{id: 1, name: 'Pooh'},
+    {id: 2, name: 'Winnie'},
+    null
+    ]
 
-  // NOTE: add router to server here
-  .use('/api', router)
+app.use(cors())
 
-  .listen(PORT, () => {
-    console.log(`Server listen on port ${PORT}`)
-  })
+// เขียน
+app.use('/api', bodyParser.json() , router);  // ต้องประกาศ
 
-app
-  .get('/', (req, res) => {
-    res.send('It\' ok!')
-  })
+router.route('/bears')
+    .get((req, res) => {
+        res.send(bears)               // ส่งรายการไปหา Client
+    })
 
+    .post(
+        (req, res) => {
+            const bear = {}
+            bear.id = bears.length + 1
+            bear.name = req.body.name   // ต้องดูว่าตรงกับค่า ภายใน obj ที่ส่งมาหรือไม่
+            bears.push(bear)            // ยัด bear ลงใน arr bears
+            res.send(bears)             // ส่งกลับไปหา client 
+    }) 
 
-/*********Another Choice****/
-let userdata=[{id:1,name:"Kittinut",surname:"Pramhan",job:"novice"}];
-var routeruser =express.Router();
-Userroute(app,userdata,routeruser);
+router.route('/bears/:id')
+    .delete( (req,res) => { 
+        const id = req.params.id-1  // ตอนส่งมาจะมีค่าเท่ากับ id
+        delete bears[id]            // แต่เราลบจาก arr เลยต้องลบ -1
+        res.send(bears)             // ส่งกลับไปหา client
+    })    
 
+    .get( (req,res) => {
+        res.send(bears[req.params.id])
+    })
+    // ไม่ต้องเขียน
+    // .put((req,res ) =>  {
+    //     const id = req.params.id
+    //     bears[id].name = req.body.name
+    //     res.send( bears)
+    // })
+app.listen(8888)
